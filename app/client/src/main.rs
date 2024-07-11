eccabb0a47be2944438b992cd6e23e6c091810c6
Serge Farny <serge.farny@gmail.com>
2024-07-11 17:40:53 +0200

    Program: add IDL

--- app/client/src/main.rs
+++ app/client/src/main.rs
@@ -8,6 +8,7 @@ use anchor_client::solana_sdk::commitment_config::CommitmentConfig;
 use anchor_client::solana_sdk::signature::Keypair;
 use anchor_client::solana_sdk::signer::EncodableKey;
 use clap::{Parser, Subcommand};
+use crate::create_pool::create_pool;
 
 #[derive(Debug, Parser)]
 #[command(name = "client")]
@@ -39,16 +40,42 @@ enum Commands {
         #[arg(short, long)]
         price: u64,
     },
+    Deposit {
+        /// pool
+        #[arg(long, value_parser = clap::value_parser!(pubkey_cli::PubkeyCli))]
+        pool: pubkey_cli::PubkeyCli,
+
+        /// amount of token to deposit into program vault
+        #[arg(short, long)]
+        amount: u64,
+    },
+    Withdraw {
+        /// pool
+        #[arg(long, value_parser = clap::value_parser!(pubkey_cli::PubkeyCli))]
+        pool: pubkey_cli::PubkeyCli,
+
+        /// withdraw token
+        #[arg(long)]
+        token: Option<bool>,
+
+        /// withdraw quote
+        #[arg(long)]
+        quote: Option<bool>,
+
+        /// amount to withdraw from program vault
+        #[arg(short, long)]
+        amount: u64,
+    },
 
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
 
     let url = args.cluster_url.parse::<Cluster>().unwrap();
     let wallet = Keypair::read_from_file(Path::new(args.keypair_path.as_str())).unwrap();
     let payer = Rc::new(wallet);
-    let client = Client::new_with_options(url.clone(), payer.clone(), CommitmentConfig::processed());
+    let client = Client::new_with_options(url.clone(), payer.clone(), CommitmentConfig::finalized());
 
     match args.command {
         Commands::CreatePool { quote, token, price } => {
@@ -57,7 +84,15 @@ fn main() {
                 price
             );
 
-
+            create_pool(&client, payer.clone(), quote.0, token.0, price)?
         },
-    }
+        Commands::Deposit { pool, amount } => {
+            anyhow::bail!("TODO")
+        },
+        Commands::Withdraw { pool, token, quote, amount } => {
+            anyhow::bail!("TODO")
+        },
+    };
+
+    Ok(())
 }
